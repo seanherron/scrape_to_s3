010370c23f4f28702075011ce3d48108a031895c
Sean Herron <seanherron@gmail.com>
2013-10-24 16:37:23 -0400

    listing keys

--- uploader/uploader.py
+++ uploader/uploader.py
@@ -6,10 +6,15 @@ import cStringIO
 conn = S3Connection()
 bucket = conn.get_bucket('faers')
 
-def s3upload(filename, url)
+def s3upload(filename, url):
     if bucket.get_key(filename) is None:
         k = Key(bucket)
         k.key = filename
         file_contents = urllib2.urlopen(url)
         fp = cStringIO.StringIO(file_contents.read())
         k.set_contents_from_file(fp)
+        
+def list_keys():
+    objects = bucket.list()
+    return objects
+    
