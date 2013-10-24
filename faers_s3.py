732abc9992dd0efc3d0d5384de4c1f655334bd8e
Sean Herron <seanherron@gmail.com>
2013-10-24 17:41:08 -0400

    successful flasking

--- faers_s3.py
+++ faers_s3.py
@@ -1,3 +1,7 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
 from scraper.scraper import get_latest_links
 from uploader.uploader import s3upload, list_keys
 
@@ -8,3 +12,13 @@ def sync_faers():
             s3upload(filename, url)
         except:
             print "Error uploading %s" % filename
+            
+@app.route("/")
+def list():
+    files = list_keys()
+    return render_template('list.html', files=files)
+
+if __name__ == "__main__":
+    app.debug = True
+    
+    app.run()
--- uploader/uploader.py
+++ uploader/uploader.py
@@ -13,11 +13,19 @@ def s3upload(filename, url):
         file_contents = urllib2.urlopen(url)
         fp = cStringIO.StringIO(file_contents.read())
         k.set_contents_from_file(fp)
+        k.set_canned_acl('public-read')
         print "Success uploading %s" % filename
     else:
         print "%s already uploaded" % filename
         
 def list_keys():
-    objects = bucket.list()
-    return objects
+    files = {}
+    bucket_list = bucket.list()
+    for item in bucket_list:
+        item.set_canned_acl('public-read')
+        url = item.generate_url(0, query_auth=False, force_http=True)
+        files[item.name] = url
+    return files
+       
+        
     
