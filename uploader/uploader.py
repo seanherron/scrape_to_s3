f792866edc3a335ab405b3cbd57f5cb691dc5e58
Sean Herron <seanherron@gmail.com>
2013-10-24 16:34:51 -0400

    inital upload code

---
+++ uploader/uploader.py
@@ -0,0 +1,15 @@
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+import urllib2
+import cStringIO
+
+conn = S3Connection()
+bucket = conn.get_bucket('faers')
+
+def s3upload(filename, url)
+    if bucket.get_key(filename) is None:
+        k = Key(bucket)
+        k.key = filename
+        file_contents = urllib2.urlopen(url)
+        fp = cStringIO.StringIO(file_contents.read())
+        k.set_contents_from_file(fp)
